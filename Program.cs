f4a0eab592d392f01d5b82a6b4e60f0d92f6af72
Wei <willy830811@gmail.com>
2022-02-24 18:54:14 +0800

    jenkins build test

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp_consumer
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World! yo!");
         }
     }
 }
